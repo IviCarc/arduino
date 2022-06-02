0c18e514754b861e0ebc1e66e5ecf2bb90e372fb
ivan-escuela <kovachivanagustin@gmail.com>
2022-06-02 16:24:28 -0300

    intento lcd con motor

--- tests/lcd-bluetooth/lcd-bluetooth.ino
+++ tests/lcd-bluetooth/lcd-bluetooth.ino
@@ -1,18 +1,24 @@
 #include <Wire.h>  
+#include <LiquidCrystal_I2C.h>
 #include <SoftwareSerial.h>
 #include <stdio.h>
 #include <ctype.h>
 
-SoftwareSerial Bluetooth(10,11);      // (TXD, RXD) of HC-05
+SoftwareSerial Bluetooth(5,6);      // (TXD, RXD) of HC-05
+LiquidCrystal_I2C lcd(0x3F,20, 4);  // SET I2C Address
 char BT_input;
 int n = 0;
 char text;
 
 void setup() 
 {
+  lcd.init();
+  lcd.backlight(); // makes Backligh ON. 
+  lcd.clear();     // Clears LCD
+  lcd.home();      // Sets cursor at Column 0 and Line 0
+  lcd.print("ROBO INDIA");  
   Bluetooth.begin(9600);
   Serial.begin(9600);
-  Serial.print("HOLA");
 }
 
 
@@ -22,9 +28,30 @@ void loop()
   {
     BT_input=(Bluetooth.read());
     Serial.write(BT_input);
-    /*if (isalpha(BT_input) != 0 || isdigit(BT_input) !=0 || BT_input == ' ') {
+      if(BT_input == '@')
+      {
+        lcd.clear(); 
+        lcd.home();
+        lcd.print("ROBO INDIA");
+        n=0;
+      }
+    if (isalpha(BT_input) != 0 || isdigit(BT_input) !=0 || BT_input == ' ')
+      {
         text = BT_input;
+        lcd.setCursor(n,1);
+        lcd.print(text);
         Serial.print(text);
-      }*/
+          if(n<15)
+          {
+            n++;
+          } 
+          else 
+          {
+            n = 0;
+            lcd.clear();
+            lcd.home();
+            lcd.print("ROBO INDIA");
+          }
+      }
   }
 }
