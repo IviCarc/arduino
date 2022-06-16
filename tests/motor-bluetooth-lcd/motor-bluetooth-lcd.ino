8057a9eba15d63b259cd5ed066879f5559017d51
Lea-Escuela <marconcinileandro2043@gmail.com>
2022-06-16 14:28:07 -0300

    Update c_cpp_properties.json

--- tests/motor-bluetooth-lcd/motor-bluetooth-lcd.ino
+++ tests/motor-bluetooth-lcd/motor-bluetooth-lcd.ino
@@ -2,11 +2,9 @@
 #include <SoftwareSerial.h>
 #include <Servo.h>
 #include <LiquidCrystal_I2C.h>
-#include <stdio.h>
-#include <ctype.h>
 
-LiquidCrystal_I2C lcd(0x3F, 20, 4); // SET I2C Address
-SoftwareSerial Bluetooth(10, 11);   // (TXD, RXD) of HC-05
+SoftwareSerial Bluetooth(10, 11); // (TXD, RXD) of HC-05
+LiquidCrystal_I2C lcd(0x27,16,2);
 char dato;
 
 // PARTE MOTOR
@@ -19,10 +17,10 @@ float DISTANCIAPASO = 0.07855; // MILIMETROS
 int stepsNEMA = 2;
 int direccionNEMA = 3;
 int pasos = 200;
-int stepsEM = 4;
-int direccionEM = 5;
+int stepsEM= 4;
+int direccionEM= 5;
 
-Servo servoMotor;
+Servo servoMotor; 
 
 void setup()
 {
@@ -38,15 +36,16 @@ void setup()
 
   digitalWrite(direccionNEMA, HIGH);
 
-  pinMode(stepsEM, OUTPUT);
-  pinMode(direccionEM, OUTPUT);
+  pinMode(stepsEM,OUTPUT);
+  pinMode(direccionEM,OUTPUT);
 
   digitalWrite(direccionEM, HIGH);
 
   lcd.init();
-  lcd.backlight(); // makes Backligh ON.
-  lcd.clear();     // Clears LCD
-  lcd.home();      // Sets cursor at Column 0 and Line 0
+  lcd.clear();
+  lcd.backlight();
+  lcd.setCursor(0,0);
+  lcd.print("LISTO");
 }
 
 // numeroTemporal guarda los numeros enviados hasta que se reciba un "/", en ese momento la variable se sobreescribe y el giro es ejecutado
@@ -57,57 +56,47 @@ int contador = 0;
 
 int ready = 0; // Booleano indica si se recibieron los 3 números
 
-void loop()
-{
-  if (Bluetooth.available())
-  {
+void loop() {
+  if (Bluetooth.available()) {
     dato = (Bluetooth.read());
-    if (dato == '/')
-    {
-      inputs[contador] = numeroTemporal.toFloat();
-      contador += 1;
-      numeroTemporal = "";
-      if (contador == 3)
-      {
-        ready = 1;
-      }
-    }
-    else
-    {
-      numeroTemporal += dato; // Añade a numeroTemporal el último carácter enviado en caso de que no sea un "/"
-    }
-
-    if (ready == 1)
-    {
-      lcd.print("Cortando");
-
-      for (int j = 0; j < inputs[0] * 48; j++)
-      { //Equivale al numero de vueltas (200 es 360º grados) o micropasos
-
-        digitalWrite(stepsEM, HIGH); // This LOW to HIGH change is what creates the
-        digitalWrite(stepsEM, LOW);  // al A4988 de avanzar una vez por cada pulso de energia.
-        delay(velocidadEM);          // Regula la velocidad, cuanto mas bajo mas velocidad.
-        servoMotor.write(45);
-      }
-
-      for (int i = 0; i < inputs[2]; i++)
-      {
-
-        for (int j = 0; j < inputs[1] * 200; j++)
-        {                                   //Equivale al numero de vueltas (200 es 360º grados) o micropasos
-          digitalWrite(stepsNEMA, HIGH);    // This LOW to HIGH change is what creates the
-          digitalWrite(stepsNEMA, LOW);     // al A4988 de avanzar una vez por cada pulso de energia.
-          delayMicroseconds(velocidadNEMA); // Regula la velocidad, cuanto mas bajo mas velocidad.
+    if (dato == '/') {
+        inputs[contador] = numeroTemporal.toFloat();
+        contador += 1;
+        numeroTemporal = "";
+        if (contador == 3) {
+            ready = 1;
         }
-        delay(1000);
+    } else {
+        numeroTemporal += dato; // Añade a numeroTemporal el último carácter enviado en caso de que no sea un "/" 
+    }
+
+  if (ready == 1) {
+    lcd.clear();
+    lcd.print("CORTANDO");
+    
+  for (int j = 0; j<inputs[0] * 48; j++) {      //Equivale al numero de vueltas (200 es 360º grados) o micropasos
+        digitalWrite(stepsEM, HIGH);  // This LOW to HIGH change is what creates the
+        digitalWrite(stepsEM, LOW); // al A4988 de avanzar una vez por cada pulso de energia.
+        delay(velocidadEM);     // Regula la velocidad, cuanto mas bajo mas velocidad.
+        servoMotor.write(45);
+   }
+
+    for (int i = 0; i < inputs[2]; i++) {
+
+      for (int j = 0; j<inputs[1] * 200; j++) {      //Equivale al numero de vueltas (200 es 360º grados) o micropasos
+        digitalWrite(stepsNEMA, HIGH);  // This LOW to HIGH change is what creates the
+        digitalWrite(stepsNEMA, LOW); // al A4988 de avanzar una vez por cada pulso de energia.
+        delayMicroseconds(velocidadNEMA);     // Regula la velocidad, cuanto mas bajo mas velocidad.
       }
-      lcd.clear();
-      lcd.print("FINALIZADO");
-      ready = 0;
-      numeroTemporal = ""; // numeroTemporal debe sobreescribirse
-      contador = 0;
-      servoMotor.write(90);
-      Serial.println("LISTO");
+      delay(1000);
+    }
+        ready = 0;
+        numeroTemporal = ""; // numeroTemporal debe sobreescribirse
+        contador = 0;
+        servoMotor.write(90);
+        Serial.println("LISTO");
+        lcd.clear();
+        lcd.print("LISTO");
     }
   }
 }
